828ee1824078246a305a32e7f61cd16dc7588429
sumesh2122 <sumeshss241@gmail.com>
2022-01-09 06:57:47 +0530

    commit check

---
+++ src/HomeAllComponets.js/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './nav';
+
+
+ function Home() {
+    return (
+        <div className='home'>
+            <NavBar/>
+        </div>
+    );
+}
+
+export default Home;
